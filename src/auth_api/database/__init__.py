b412aad00ee3fe25ca4148c9770c4042667b7002
Afterstone <invisiblex@gmail.com>
2022-12-26 04:20:50 +0100

    Set up basic SA stuff

--- src/auth_api/database/__init__.py
+++ src/auth_api/database/__init__.py
@@ -1,5 +1,6 @@
-from ._setup import Session
+from ._setup import Session, get_db
 
 __all__ = [
+    "get_db",
     "Session"
 ]
--- src/auth_api/database/_setup.py
+++ src/auth_api/database/_setup.py
@@ -1,8 +1,22 @@
+import typing as t
+
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from auth_api.config import AUTH_DB_CONNECTION_STRING, VERBOSE
 
 ENGINE = create_engine(AUTH_DB_CONNECTION_STRING, echo=VERBOSE)
 
-Session = sessionmaker(bind=ENGINE)
+SessionMaker = sessionmaker(bind=ENGINE)
+
+Base = declarative_base()
+
+
+def get_db() -> t.Generator[Session, None, None]:
+    session = SessionMaker()
+    try:
+        yield session
+    finally:
+        session.close()
