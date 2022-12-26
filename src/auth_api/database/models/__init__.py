b24184f87bd522289d15b05650c31f7d30201a1b
Afterstone <invisiblex@gmail.com>
2022-12-26 07:58:10 +0100

    Set up database models

---
+++ src/auth_api/database/models/__init__.py
@@ -0,0 +1,23 @@
+import datetime as dt
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+
+from .. import Base
+
+
+class AuthUser(Base):
+    __tablename__ = 'auth_user'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(64), nullable=False, unique=True, index=True)
+    email = Column(String(255), nullable=False, unique=True, index=True)
+    password_hash = Column(String(128), nullable=False)
+
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
+    created_at = Column(DateTime, nullable=False, default=dt.datetime.utcnow)
+    updated_at = Column(DateTime, nullable=True, onupdate=dt.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User('{self.username}', '{self.email}')>"
